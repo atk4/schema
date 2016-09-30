1beb1ba8e2677ab0ea4f288e1fb9541ccf71aaee
Romans Malinovskis <romans@agiletoolkit.org>
2016-09-30 13:31:46 +0100

    add inital cocde

---
+++ tests/SQLTestcaseTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace atk4\schema\tests;
+
+class SQLTestcaseTest extends \atk4\schema\PHPUnit_SchemaTestCase
+{
+    public function testInit()
+    {
+        $this->setDB($q = ['user' => [
+            ['name' => 'John', 'surname' => 'Smith'],
+            ['name' => 'Steve', 'surname' => 'Jobs'],
+        ]]);
+
+        $q2 = $this->getDB('user');
+
+        $this->setDB($q2);
+        $q3 = $this->getDB('user');
+
+        $this->assertEquals($q2, $q3);
+
+        $this->assertEquals($q, $this->getDB('user', true));
+    }
+}
