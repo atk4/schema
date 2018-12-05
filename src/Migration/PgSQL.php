8182cbb996feaaf84025cabe362a4246ec0c59ef
DarkSide <imants.horsts@inbox.lv>
2018-12-06 01:32:12 +0200

    PostGre suppport

--- src/Migration/PgSQL.php
+++ src/Migration/PgSQL.php
@@ -5,4 +5,6 @@ namespace atk4\schema\Migration;
 // NOT IMPLEMENTED !!!
 class PgSQL extends \atk4\schema\Migration
 {
+    /** @var string Expression to create primary key */
+    public $primary_key_expr = 'serial primary key';
 }
