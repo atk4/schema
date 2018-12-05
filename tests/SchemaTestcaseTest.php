86a17936f3321affe62e0485cff8869f54a67181
Imants Horsts <DarkSide666@users.noreply.github.com>
2018-12-05 19:03:52 +0200

    Update SchemaTestcaseTest.php

--- tests/SchemaTestcaseTest.php
+++ tests/SchemaTestcaseTest.php
@@ -2,7 +2,7 @@
 
 namespace atk4\schema\tests;
 
-class SchemaTestcaseTest extends \atk4\schema\PHPUnit_SchemaTestCase
+class SchemaTestcaseTest extends \atk4\core\PHPUnit_AgileTestCase
 {
     public function testInit()
     {
