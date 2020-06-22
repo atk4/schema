d61e841ee1363f035e2ded6755473be7d59fa7f8
Georgi Hristov <georgehristov@users.noreply.github.com>
2020-06-22 10:01:12 +0200

    [update] use Stopwatch\Connection class (#65)

--- demos/init.php
+++ demos/init.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 include '../vendor/autoload.php';
 $db = \atk4\data\Persistence::connect('mysql://root:root@localhost/test');
 
-$db->connection = new \atk4\dsql\Connection_Dumper(['connection' => $db->connection]);
+$db->connection = new \atk4\dsql\Debug\Stopwatch\Connection(['connection' => $db->connection]);
